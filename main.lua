1a65143be4bda714641d8a625ce73a5a53e1d934
tip <103219982+tip52@users.noreply.github.com>
2025-03-14 18:08:49 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,17 +3,59 @@ I made this because I was sick of there not being an easy solution
 
 REQUIRED FUNCS:
 
-gettenv
+gettenv (Overwritable): getgenv().overwrite_gettenv = true
+
 getreg
+
+getgc (not needed unless using getthreadfunctions)
 
 ]]
 
+local function getScriptFromPath(path)
+    local obj = game
+    for segment in path:gmatch("[^.]+") do
+        obj = obj:FindFirstChild(segment)
+        if not obj then return nil end
+    end
+    return obj
+end
+
+local _GCache = {}
+local sharedCache = {}
+
+for i, v in ipairs(getreg()) do
+    if type(v) == "table" then
+        if i == 1 then
+            _GCache = v
+        elseif i == 4 then
+            sharedCache = v
+            break
+        end
+    end
+end
 
 local ThreadLib = {}
 
-function ThreadLib.getscriptfromthread(thread)
-    return gettenv(thread).script
-end 
+-- Due to this only returning script, shared, and _G, and not the actual thread env, there is an option to overwrite or not.
+if getgenv().overwrite_gettenv then
+    local threadEnv = {}
+    function ThreadLib.gettenv(thread)
+        threadEnv = {}
+        local source = debug.info(thread, 1, "s")
+        threadEnv.script = nil
+        threadEnv.shared = getrenv and getrenv().shared or sharedCache
+        threadEnv._G = getrenv and getrenv()._G or _GCache
+
+        if source then
+            local scriptInstance = getScriptFromPath(source)
+            threadEnv.script = scriptInstance or nil
+        end
+
+        return threadEnv
+    end
+else
+    ThreadLib.gettenv = gettenv
+end
 
 local threads = {}
 function ThreadLib.getallthreads()
@@ -25,24 +67,56 @@ function ThreadLib.getallthreads()
     end
     return threads
 end
+
 ThreadLib.getthreads = ThreadLib.getallthreads
+
+
+function ThreadLib.getscriptfromthread(thread)
+    return ThreadLib.gettenv(thread).script
+end
+
+ThreadLib.getscriptthread = ThreadLib.getscriptfromthread
+
 
 local connectedThreads = {}
 function ThreadLib.getscriptthreads(script)
     connectedThreads = {}
     for i, v in next, ThreadLib.getthreads() do
         if ThreadLib.getscriptfromthread(v) == script then
-            table.insert(connectedThreads,v)
+            table.insert(connectedThreads, v)
         end
     end
     return connectedThreads
-end 
+end
 
 function ThreadLib.getfunctionthreads(func)
     local script = getfenv(func).script
     return ThreadLib.getscriptthreads(script)
-end 
+end
+
+if getgc then
+    local threadfuncs = {}
+    function ThreadLib.getthreadfunctions(thread)
+        threadfuncs = {}
+        local script = ThreadLib.gettenv(thread).script
+
+        for _, v in next, getgc() do
+            if typeof(v) == "function" then
+                local sourceScript = getfenv(v).script
+                if sourceScript == script then
+                    table.insert(threadfuncs, v)
+                end
+            end
+        end
+
+        return threadfuncs
+    end
+end
+
+
+
+-- add to the environment
 
 for i, v in next, ThreadLib do
     getfenv()[i] = v
-end 
+end
